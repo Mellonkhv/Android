34a5f5eb591ec4ca37fc3e12fd2f388eb4972ce1
Mellonkhv <mellonkhv@gmail.com>
2016-06-21 16:39:49 +1000

    Workout
    Второй фрагмент

---
+++ Workout/app/src/main/java/com/mellonkhv/workout/WorkoutListFragment.java
@@ -0,0 +1,28 @@
+package com.mellonkhv.workout;
+
+
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+public class WorkoutListFragment extends ListFragment {
+
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+        
+    }
+
+}
