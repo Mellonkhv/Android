bd2044d063d6c96c3b0a8765774f6214e4bc66ef
Mellonkhv <mellonkhv@gmail.com>
2016-07-04 10:02:38 +1000

    Workout
    Активности разделены на вариант для телефона и планшета

--- Workout/app/src/main/java/com/mellonkhv/workout/MainActivity.java
+++ Workout/app/src/main/java/com/mellonkhv/workout/MainActivity.java
@@ -1,8 +1,10 @@
 package com.mellonkhv.workout;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListener {
 
@@ -15,12 +17,19 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id){
-        WorkoutDetailFragment details = new WorkoutDetailFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction(); // Начало транзакции
-        details.setWorkout(id); // Получаем конкретный фрагмент
-        transaction.replace(R.id.fragment_container, details); // Заменяем фрагмент
-        transaction.addToBackStack(null); // Добавляем фрагмент в стек возврата
-        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE); // Анимируем переход
-        transaction.commit(); // Подтверддаем транзакцию
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer != null) {
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction(); // Начало транзакции
+            details.setWorkout(id); // Получаем конкретный фрагмент
+            transaction.replace(R.id.fragment_container, details); // Заменяем фрагмент
+            transaction.addToBackStack(null); // Добавляем фрагмент в стек возврата
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE); // Анимируем переход
+            transaction.commit(); // Подтверддаем транзакцию
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
     }
 }
